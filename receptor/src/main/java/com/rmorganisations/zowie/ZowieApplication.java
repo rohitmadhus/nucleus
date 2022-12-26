a40d03d8b5d2549277df2ed35d191e45afcb47c0
Rohit Madhu <mailrohitmadhu@gmail.com>
2022-12-27 04:43:24 +0530

    first commit

---
+++ receptor/src/main/java/com/rmorganisations/zowie/ZowieApplication.java
@@ -0,0 +1,13 @@
+package com.rmorganisations.zowie;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZowieApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZowieApplication.class, args);
+	}
+
+}
