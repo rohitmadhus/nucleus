6a5e6d12765b796acb9c8455a08e73391aaec4da
Rohit Madhu <mailrohitmadhu@gmail.com>
2023-01-23 03:44:36 +0530

    first commit

---
+++ admin-portal-service/src/main/java/com/zowie/adminportalservice/repository/TileTypeRepository.java
@@ -0,0 +1,11 @@
+package com.zowie.adminportalservice.repositories;
+
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface TileTypeRepository  {
+
+
+}
